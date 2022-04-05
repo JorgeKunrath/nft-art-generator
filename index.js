455b64514385d8566838d356bcb44fee4f9e6ae3
JorgeKunrath <54911848+JorgeKunrath@users.noreply.github.com>
2022-04-05 00:39:53 -0300

    basic cleanup for our project

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const { readFileSync, writeFileSync, readdirSync, rmSync, existsSync, mkdirSync } = require('fs');
-const sharp = require('sharp');
+const {
+  readFileSync,
+  writeFileSync,
+  readdirSync,
+  rmSync,
+  existsSync,
+  mkdirSync,
+} = require('fs')
+const sharp = require('sharp')
 
 const template = `
     <svg width="256" height="256" viewBox="0 0 256 256" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -13,111 +20,89 @@ const template = `
     </svg>
 `
 
-const takenNames = {};
-const takenFaces = {};
-let idx = 999;
+const takenFaces = {}
+let idx = 10
 
-function randInt(max) {
-    return Math.floor(Math.random() * (max + 1));
-}
+const randInt = (max) => Math.floor(Math.random() * (max + 1))
+// const randElement = (arr) => arr[Math.floor(Math.random() * arr.length)]
 
-function randElement(arr) {
-    return arr[Math.floor(Math.random() * arr.length)];
-}
-
-
-function getRandomName() {
-    const adjectives = 'fired trashy tubular nasty jacked swol buff ferocious firey flamin agnostic artificial bloody crazy cringey crusty dirty eccentric glutinous harry juicy simple stylish awesome creepy corny freaky shady sketchy lame sloppy hot intrepid juxtaposed killer ludicrous mangy pastey ragin rusty rockin sinful shameful stupid sterile ugly vascular wild young old zealous flamboyant super sly shifty trippy fried injured depressed anxious clinical'.split(' ');
-    const names = 'aaron bart chad dale earl fred grady harry ivan jeff joe kyle lester steve tanner lucifer todd mitch hunter mike arnold norbert olaf plop quinten randy saul balzac tevin jack ulysses vince will xavier yusuf zack roger raheem rex dustin seth bronson dennis'.split(' ');
-    
-    const randAdj = randElement(adjectives);
-    const randName = randElement(names);
-    const name =  `${randAdj}-${randName}`;
-
-
-    if (takenNames[name] || !name) {
-        return getRandomName();
-    } else {
-        takenNames[name] = name;
-        return name;
-    }
-}
-
-function getLayer(name, skip=0.0) {
-    const svg = readFileSync(`./layers/${name}.svg`, 'utf-8');
-    const re = /(?<=\<svg\s*[^>]*>)([\s\S]*?)(?=\<\/svg\>)/g
-    const layer = svg.match(re)[0];
-    return Math.random() > skip ? layer : '';
+function getLayer(name, skip = 0.0) {
+  if (!Math.random() > skip) return ''
+  const svg = readFileSync(`./layers/${name}.svg`, 'utf-8')
+  const re = /(?<=\<svg\s*[^>]*>)([\s\S]*?)(?=\<\/svg\>)/g
+  const layer = svg.match(re)[0]
+  return layer
 }
 
 async function svgToPng(name) {
-    const src = `./out/${name}.svg`;
-    const dest = `./out/${name}.png`;
+  const src = `./out/${name}.svg`
+  const dest = `./out/${name}.png`
 
-    const img = await sharp(src);
-    const resized = await img.resize(1024);
-    await resized.toFile(dest);
+  const img = sharp(src)
+  const resized = img.resize(1024)
+  await resized.toFile(dest)
 }
 
+async function createImage(idx) {
+  // TODO: insert rarity
+  // TODO: check for layers exclusions
+  const bg = randInt(5)
+  const hair = randInt(7)
+  const eyes = randInt(9)
+  const nose = randInt(4)
+  const mouth = randInt(5)
+  const beard = randInt(3)
 
-function createImage(idx) {
+  // FIXME: verify if it works when randInts are bigger than 9
+  const face = [hair, eyes, mouth, nose, beard].join('')
 
-    const bg = randInt(5);
-    const hair = randInt(7);
-    const eyes = randInt(9);
-    const nose = randInt(4); 
-    const mouth = randInt(5);
-    const beard = randInt(3);
-    // 18,900 combinations
+  if (face[takenFaces]) {
+    createImage()
+  } else {
+    face[takenFaces] = face
 
-    const face = [hair, eyes, mouth, nose, beard].join('');
+    const final = template
+      .replace('<!-- bg -->', getLayer(`bg${bg}`))
+      .replace('<!-- head -->', getLayer('head0'))
+      .replace('<!-- hair -->', getLayer(`hair${hair}`))
+      .replace('<!-- eyes -->', getLayer(`eyes${eyes}`))
+      .replace('<!-- nose -->', getLayer(`nose${nose}`))
+      .replace('<!-- mouth -->', getLayer(`mouth${mouth}`))
+      .replace('<!-- beard -->', getLayer(`beard${beard}`, 0.5))
 
-    if (face[takenFaces]) {
-        createImage();
-    } else {
-        const name = getRandomName()
-        console.log(name)
-        face[takenFaces] = face;
-
-        const final = template
-            .replace('<!-- bg -->', getLayer(`bg${bg}`))
-            .replace('<!-- head -->', getLayer('head0'))
-            .replace('<!-- hair -->', getLayer(`hair${hair}`))
-            .replace('<!-- eyes -->', getLayer(`eyes${eyes}`))
-            .replace('<!-- nose -->', getLayer(`nose${nose}`))
-            .replace('<!-- mouth -->', getLayer(`mouth${mouth}`))
-            .replace('<!-- beard -->', getLayer(`beard${beard}`, 0.5))
-
-        const meta = {
-            name,
-            description: `A drawing of ${name.split('-').join(' ')}`,
-            image: `${idx}.png`,
-            attributes: [
-                { 
-                    beard: '',
-                    rarity: 0.5
-                }
-            ]
-        }
-        writeFileSync(`./out/${idx}.json`, JSON.stringify(meta))
-        writeFileSync(`./out/${idx}.svg`, final)
-        svgToPng(idx)
+    const meta = {
+      name: `${idx}`,
+      description: '',
+      image: `${idx}.png`,
+      attributes: [
+        {
+          beard: '',
+          rarity: 0.5,
+        },
+      ],
     }
-
-
+    writeFileSync(`./out/${idx}.json`, JSON.stringify(meta))
+    writeFileSync(`./out/${idx}.svg`, final)
+    await svgToPng(idx)
+  }
 }
 
+function setup() {
+  // Create dir if not exists
+  if (!existsSync('./out')) {
+    mkdirSync('./out')
+  }
 
-// Create dir if not exists
-if (!existsSync('./out')){
-    mkdirSync('./out');
+  // Cleanup dir before each run
+  readdirSync('./out').forEach((f) => rmSync(`./out/${f}`))
 }
 
-// Cleanup dir before each run
-readdirSync('./out').forEach(f => rmSync(`./out/${f}`));
+async function run() {
+  setup()
+  while (idx > 0) {
+    await createImage(idx)
+    idx--
+  }
+}
 
-
-do {
-    createImage(idx);
-    idx--;
-  } while (idx >= 0);
+run()
